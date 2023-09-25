6af6e8fd9a5f4524efdd9b0de191edb2dbdc4044
Bruno Freire <brunofreire1717@gmail.com>
2023-09-24 20:35:57 -0500

    feature: add DTO in UserController

--- src/infra/web/controllers/UserController.ts
+++ src/infra/web/controllers/UserController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { CreateUserUseCase } from '../../../application/useCases/CreateUserUseCase';
 import { UpdateUserProfileUseCase } from '../../../application/useCases/UpdateUserProfileUseCase';
+import { CreateUserDTO } from '../../../presentation/dtos/CreateUserDTO';
+import { UpdateUserProfileDTO } from '../../../presentation/dtos/UpdateUserProfileDTO';
 
 class UserController {
   constructor(
@@ -9,14 +11,14 @@ class UserController {
   ) {}
 
   async createUser(req: Request, res: Response) {
-    const { name, email } = req.body;
+    const {  } = req.body as CreateUserDTO;
     const user = await this.createUserUseCase.execute(name, email);
     return res.json(user);
   }
 
   async updateUserProfile(req: Request, res: Response) {
     const { userId } = req.params;
-    const { name, email } = req.body;
+    const { name, email } = req.body as UpdateUserProfileDTO;
     const updatedUser = await this.updateUserProfileUseCase.execute(userId, name, email);
     return res.json(updatedUser);
   }
