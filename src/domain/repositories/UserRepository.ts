fc7fd17de30cb775a33463cc1ce84998bd8534f0
Bruno Freire <108240079+Bruno-freire@users.noreply.github.com>
2023-09-27 16:45:19 -0500

    Update UserRepository.ts

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -2,7 +2,9 @@ import { User } from '../entities/User';
 
 interface UserRepository {
   createUser(name: string, email: string): Promise<User>;
-  updateUserProfile(userId: string, name: string, email: string): Promise<User>;
+  updateUserProfile(id: string, name: string, email: string): Promise<User>;
+  getUsers(): Promise<User>;
+  deleteUser(id: number): Promise<User>;
 }
 
 export type { UserRepository };
