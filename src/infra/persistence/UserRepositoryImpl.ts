95820580ab1be26f21b554d3f7ae3c336283993b
Bruno Freire <108240079+Bruno-freire@users.noreply.github.com>
2023-09-27 17:08:29 -0500

    Feature: UserRepositoryImpl with array the users

--- src/infra/persistence/UserRepositoryImpl.ts
+++ src/infra/persistence/UserRepositoryImpl.ts
@@ -3,18 +3,29 @@ import { User } from '../../domain/entities/User';
 import { injectable } from 'tsyringe';
 
 let accountant = 1
+let usersArray: User[] = []
 
 @injectable()
 class UserRepositoryImpl implements UserRepository {
   async createUser(name: string, email: string): Promise<User> {
     const user = new User(`${accountant}`, name, email);
+    usersArray.push(user)
     accountant++
     return user;
+  }
+
+  async getUsers(): Promise<User[]> {
+    return usersArray
   }
 
   async updateUserProfile(userId: string, name: string, email: string): Promise<User> {
     const updatedUser = new User(userId, name, email);
     return updatedUser;
+  }
+
+  async deleteUser(id: number): Promise<User[]> {
+    usersArray = usersArray.filter(data => data.id !== id)
+    return usersArray
   }
 }
 
