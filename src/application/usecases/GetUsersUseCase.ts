547cd7fabad906130bbefdc91cf61190c46d8e15
Bruno Freire <108240079+Bruno-freire@users.noreply.github.com>
2023-09-27 17:01:32 -0500

    Update GetUsersUseCase.ts

--- src/application/usecases/GetUsersUseCase.ts
+++ src/application/usecases/GetUsersUseCase.ts
@@ -1,1 +1,14 @@
+import { inject, injectable } from 'tsyringe';
+import { UserRepository } from '../../domain/repositories/UserRepository';
 
+@injectable()
+class GetUsersUseCase {
+  constructor(@inject("UserRepositoryImpl") private userRepository: UserRepository) {}
+
+  async execute() {
+    const users = await this.userRepository.getUsers();
+    return users;
+  }
+}
+
+export { GetUsersUseCase };
